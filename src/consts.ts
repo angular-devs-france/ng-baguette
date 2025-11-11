5d35fbeafc0fe452873310d04324617f6e88984e
Yann-thomas Le Moigne <yann.tho.le.moigne@gmail.com>
2025-11-11 20:19:27 +0100

    Epitech

--- src/consts.ts
+++ src/consts.ts
@@ -12,19 +12,19 @@ export const CFP_END_DATE = "2026-03-14";
 export const OPEN_PROGRAM_DATE = "2026-04-01"; // Date when the agenda will be made public
 export const SPONSOR_ENDING_RESEARCH_DATE = "2026-04-20";
 export const EVENT_YEAR = new Date(EVENT_DATE).getFullYear();
-export const BLIND_TICKET_PRICE = 25; // in euros
+export const BLIND_TICKET_PRICE = 49; // in euros
 export const OPEN_BLIND_TICKET_SALE_DATE = "2026-02-02";
 export const BLIND_TICKET_SOLD_OUT: boolean = false;
-export const TICKET_PRICE = 30; // in euros
-export const OPEN_TICKET_SALE_DATE = "2026-03-02";
+export const TICKET_PRICE = 69; // in euros
+export const OPEN_TICKET_SALE_DATE = "2026-04-01";
 export const TICKET_SOLD_OUT: boolean = false;
 
 
 export const ADDRESS = {
-  streetAddress: "3 rue Lavoisier",
-  addressLocality: "Nantes",
-  addressRegion: "Pays de la Loire",
-  postalCode: "44300",
+  streetAddress: "24 Rue Pasteur",
+  addressLocality: "Le Kremlin-Bicêtre",
+  addressRegion: "Ile de France",
+  postalCode: "94270",
   addressCountry: "France",
 };
 
@@ -61,51 +61,36 @@ export const CFP_LINK = "https://conference-hall.io/ng-baguette-conf-2026";
 export const HELLOASSO_LINK = "https://www.helloasso.com/associations/angular-nexus/evenements/ng-baguette-conf-2026";
 
 export const SPONSORS: Array<{ name: string; photos: string; url: string; }> = [
-  {
-    name: "eezyr",
-    photos: "/sponsors/eezyR.png",
-    url: "https://eezyr.com/"
-  },
-  {
-    name: "/sponsors/house-of-angular.png",
-    photos: "/sponsors/house-of-angular.png",
-    url: "https://houseofangular.io/"
-  },
-  {
-    name: "Lucca",
-    photos: "/sponsors/lucca_light.svg",
-    url: "https://lucca-carriere.vercel.app/"
-  },
-  {
-    name: "SFEIR",
-    photos: "/sponsors/sfeir_light.png",
-    url: "https://sfeir.com/"
-  },
-  {
-    name: "Takima",
-    photos: "/sponsors/takima.png",
-    url: "https://www.takima.fr/"
-  }
+  // {
+  //   name: "eezyr",
+  //   photos: "/sponsors/eezyR.png",
+  //   url: "https://eezyr.com/"
+  // },
+  // {
+  //   name: "/sponsors/house-of-angular.png",
+  //   photos: "/sponsors/house-of-angular.png",
+  //   url: "https://houseofangular.io/"
+  // },
 ]
 export const PARTENERS: Array<{ name: string; photos: string; url: string }> = [
-  {
-    name: "NantesJS",
-    photos: "/sponsors/nantesjs.svg",
-    url: "https://nantesjs.org/"
-  },
-  {
-    name: 'Stickergreen',
-    photos: "/sponsors/stickergreen.png",
-    url: "https://stickergreen.com/"
-  },
-  {
-    name: "Angular Love",
-    photos: "/sponsors/angular-love.svg",
-    url: "https://angular.love/"
-  },
-  {
-    name: "Paris Angular",
-    photos: "/sponsors/paris-angular.png",
-    url: "https://www.meetup.com/paris-angular/"
-  }
+  // {
+  //   name: "NantesJS",
+  //   photos: "/sponsors/nantesjs.svg",
+  //   url: "https://nantesjs.org/"
+  // },
+  // {
+  //   name: 'Stickergreen',
+  //   photos: "/sponsors/stickergreen.png",
+  //   url: "https://stickergreen.com/"
+  // },
+  // {
+  //   name: "Angular Love",
+  //   photos: "/sponsors/angular-love.svg",
+  //   url: "https://angular.love/"
+  // },
+  // {
+  //   name: "Paris Angular",
+  //   photos: "/sponsors/paris-angular.png",
+  //   url: "https://www.meetup.com/paris-angular/"
+  // }
 ]
