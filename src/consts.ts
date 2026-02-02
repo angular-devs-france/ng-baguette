6433de60a0da5e79514b4595ea3bc7ee5bfc8182
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2026-02-02 13:21:46 +0100

    fix: add sponsorship english version

--- src/consts.ts
+++ src/consts.ts
@@ -28,7 +28,7 @@ export const TICKET_SOLD_OUT: boolean = false;
 export const TICKETING_TRIGGER_EVENT: "awaiting" | "open" | "closed" = "open";
 
 export const SPONSORSHIP_BROCHURE_URL = {
-  en: "/pdf/dossier-sponsoring-ngbaguette-2026.pdf",
+  en: "/pdf/sponsorship-prospectus-ngbaguette-2026.pdf",
   fr: "/pdf/dossier-sponsoring-ngbaguette-2026.pdf",
 };
 
